ced5844fe2f3c7a22f9888c6299d34e878324dd4
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-17 14:01:52 -0400

    initial commit of the completed google OAuth Project using Django framework

--- oauth_practiceII/settings.py
+++ oauth_practiceII/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # new
+                # new for oauth
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
             ],
